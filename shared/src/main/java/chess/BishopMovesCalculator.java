1469db337e294a48c77f608b1f337bf4d27f568b
Abigail Peterson <desrange@byu.edu>
2025-01-23 11:01:49 -0700

    Added a startPosition local variable to BishopMovesCalculator for clarity.

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -4,14 +4,17 @@ import java.util.Collection;
 
 public class BishopMovesCalculator extends PieceMovesCalculator {
     private Collection<ChessMove> bishopMoves;
+    private ChessPosition startPosition;
 
     public BishopMovesCalculator() {
         this.bishopMoves = null;
+        this.startPosition = null;
     }
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard chessboard, ChessPosition position) {
         // finds the positions that the bishop can move to and adds them to the Collection bishopMoves
+        startPosition = position;
         return bishopMoves;
     }
 }
