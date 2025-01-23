44c698d39c475f4020d889db0abc1da0c124a43e
Abigail Peterson <desrange@byu.edu>
2025-01-23 12:04:20 -0700

    Started validating moves in the BishopMovesCalculator. Finished while loop to check all spaces in the upward right direction.

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -14,7 +14,24 @@ public class BishopMovesCalculator extends PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard chessboard, ChessPosition position) {
         // finds the positions that the bishop can move to and adds them to the Collection bishopMoves
+        // initialized local variables
         startPosition = position;
+        int col = position.getColumn();
+        int row = position.getRow();
+        // four directions to go: up and right, up and left, down and left, down and right
+        //check up and right to the edge of the board
+        while (row < 9 && col < 9) {
+            //set new ChessPosition
+            position = new ChessPosition(row, col);
+            if (chessboard.getPiece(position) == null) {
+                //if the space is free, add it to the collection
+                bishopMoves.add(new ChessMove(startPosition, position, ChessPiece.PieceType.BISHOP));
+            }
+            //increment both row and col
+            row++;
+            col++;
+        }
+
         return bishopMoves;
     }
 }
