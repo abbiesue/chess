d281b79872da0728fff153c7c8d98460caca7e61
Abigail Peterson <desrange@byu.edu>
2025-01-24 12:08:52 -0700

    added PawnMovesCalculator class

---
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -0,0 +1,16 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class PawnMovesCalculator implements PieceMovesCalculator {
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard chessboard, ChessPosition position) {
+        List<ChessMove> pawnMoves = new ArrayList<>();
+
+        // if this is the first time this piece has moved,
+
+        return pawnMoves;
+    }
+}
