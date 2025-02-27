364ea554e24d97263028d0e4491744269724d388
Abigail Peterson <desrange@byu.edu>
2025-02-27 15:01:02 -0700

    Created handlers and started registering an endpoint

---
+++ server/src/main/java/handler/UserHandler.java
@@ -0,0 +1,10 @@
+package handler;
+
+import spark.Request;
+import spark.Response;
+
+public class UserHandler {
+    public Object register(Request req, Response res) {
+        return null;
+    }
+}
