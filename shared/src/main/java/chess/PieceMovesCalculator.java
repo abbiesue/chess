99f042eb1462f8ac42211989c7641490d7a289e0
Abigail Peterson <desrange@byu.edu>
2025-03-04 19:13:52 -0700

    changed const nameing in PieceMovesCalculator

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -5,21 +5,21 @@ import java.util.Collection;
 import java.util.List;
 
 public interface PieceMovesCalculator {
-    List<ChessMove> moves = new ArrayList<>();
-    ChessPosition startPosition = new ChessPosition(0,0);
-    ChessBoard chessBoard = new ChessBoard();
+    List<ChessMove> MOVES = new ArrayList<>();
+    ChessPosition START_POSITION = new ChessPosition(0,0);
+    ChessBoard CHESS_BOARD = new ChessBoard();
 
     Collection<ChessMove> pieceMoves(ChessBoard chessboard, ChessPosition position);
     public static boolean checkNAdd(int row, int col) {
         ChessPosition position = new ChessPosition(row, col);
-        if (chessBoard.getPiece(position) != null) {
-            if (chessBoard.getPiece(position).getTeamColor() != chessBoard.getPiece(startPosition).getTeamColor()) {
-                moves.add(new ChessMove(startPosition, position, null));
+        if (CHESS_BOARD.getPiece(position) != null) {
+            if (CHESS_BOARD.getPiece(position).getTeamColor() != CHESS_BOARD.getPiece(START_POSITION).getTeamColor()) {
+                MOVES.add(new ChessMove(START_POSITION, position, null));
             }
             return false;
         }
-        if (chessBoard.getPiece(position) == null) {
-            moves.add(new ChessMove(startPosition, position, null));
+        if (CHESS_BOARD.getPiece(position) == null) {
+            MOVES.add(new ChessMove(START_POSITION, position, null));
         }
         return true;
     }
