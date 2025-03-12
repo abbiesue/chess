5fd23cd89e077f7d62f9cd5bde109f6a37dfedf7
Abigail Peterson <desrange@byu.edu>
2025-03-12 13:21:44 -0600

    finished writing implementation for all SQLAuthDAO methods. Created test file, but still needs to be tested.

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,10 +1,11 @@
 package dataaccess;
 
 import model.AuthData;
+import server.ResponseException;
 
 public interface AuthDAO {
-    void createAuth(AuthData authdata);
-    AuthData getAuth(String authtoken);
-    void deleteAuth(String authToken);
-    void clear();
+    void createAuth(AuthData authdata) throws ResponseException;
+    AuthData getAuth(String authtoken) throws ResponseException;
+    void deleteAuth(String authToken) throws ResponseException;
+    void clear() throws ResponseException;
 }
--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,12 +1,16 @@
 package dataaccess;
 
 import model.AuthData;
+import model.UserData;
 import server.ResponseException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SQLAuthDAO extends SQLDAO implements AuthDAO{
     private String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS userData (
+            CREATE TABLE IF NOT EXISTS authData (
             `authToken` varchar(256) NOT NULL,
             `username` varchar(256) NOT NULL,
             PRIMARY KEY(`username`),
@@ -18,22 +22,46 @@ public class SQLAuthDAO extends SQLDAO implements AuthDAO{
     public SQLAuthDAO() throws ResponseException, DataAccessException {configureDatabase(createStatements);}
 
     @Override
-    public void createAuth(AuthData authdata) {
-
+    public void createAuth(AuthData authdata) throws ResponseException {
+        var statement = "INSERT INTO authData (authToken, username) VALUES (?,?)";
+        String authToken = authdata.authToken();
+        String username = authdata.username();
+        executeUpdate(statement, authToken, username);
     }
 
     @Override
-    public AuthData getAuth(String authtoken) {
+    public AuthData getAuth(String authToken) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, authToken FROM authData WHERE username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
-    public void deleteAuth(String authToken) {
-
+    public void deleteAuth(String authToken) throws ResponseException {
+        var statement = "DELETE FROM pet WHERE authToken=?";
+        executeUpdate(statement, authToken);
     }
 
     @Override
-    public void clear() {
+    public void clear() throws ResponseException {
+        var statement = "TRUNCATE authData";
+        executeUpdate(statement);
+    }
 
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var authToken = rs.getString("authToken");
+        return new AuthData(authToken, username);
     }
 }
