1fba2e95097fe9ff58db7f50cfb913a26cb1c160
Abigail Peterson <desrange@byu.edu>
2025-03-12 23:56:08 -0600

    Unit test suite updated

--- server/src/test/java/dataaccess/SQLAuthDAOTests.java
+++ server/src/test/java/dataaccess/SQLAuthDAOTests.java
@@ -1,4 +1,51 @@
 package dataaccess;
 
+import model.AuthData;
+import org.junit.jupiter.api.*;
+import server.ResponseException;
+
 public class SQLAuthDAOTests {
+    private SQLAuthDAO authDAO;
+    AuthData existingAuth = new AuthData("authToken", "username");
+
+    @BeforeEach
+    void init() throws ResponseException, DataAccessException {
+        authDAO = new SQLAuthDAO();
+    }
+
+    @AfterEach
+    void tearDown() throws ResponseException {
+        authDAO.clear();
+    }
+
+    @Test
+    @DisplayName("createAuth success")
+    public void createAuthSuccess() {
+        Assertions.assertDoesNotThrow(()->authDAO.createAuth(existingAuth));
+    }
+
+    @Test
+    @DisplayName("getAuth success")
+    public void getAuthSuccess() {
+        Assertions.assertDoesNotThrow(()->authDAO.getAuth(existingAuth.authToken()));
+    }
+
+    @Test
+    @DisplayName("getAuth failure")
+    public void getAuthFailure() throws ResponseException {
+        Assertions.assertNull(authDAO.getAuth("nonToken"));
+    }
+
+    @Test
+    @DisplayName("deleteAuth success")
+    public void deleteAuthSuccess() {
+        Assertions.assertDoesNotThrow(()->{authDAO.deleteAuth(existingAuth.authToken());});
+    }
+
+    @Test
+    @DisplayName("clear success")
+    public void clearSuccess() {
+        Assertions.assertDoesNotThrow(()->{authDAO.clear();});
+    }
+
 }
--- server/src/test/java/dataaccess/SQLGameDAOTests.java
+++ server/src/test/java/dataaccess/SQLGameDAOTests.java
@@ -1,4 +1,95 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import server.ResponseException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class SQLGameDAOTests {
+    private SQLGameDAO gameDAO;
+
+    @BeforeEach
+    void init() throws ResponseException, DataAccessException {
+        gameDAO = new SQLGameDAO();
+    }
+
+    @AfterEach
+    void tearDown() throws ResponseException {
+        gameDAO.clear();
+    }
+
+    @Test
+    @DisplayName("createGame success")
+    public void createGameSuccess() {
+        Assertions.assertDoesNotThrow(()->gameDAO.createGame("gameName"));
+    }
+
+    @Test
+    @DisplayName("createGame failure")
+    public void createGameFailure() {
+        Assertions.assertThrows(ResponseException.class, ()->{gameDAO.createGame(null);});
+    }
+
+    @Test
+    @DisplayName("getGame success")
+    public void getGameSuccess() throws ResponseException {
+        GameData game = gameDAO.createGame("gameName");
+        Assertions.assertDoesNotThrow(()->gameDAO.getGame(game.gameID()));
+    }
+
+    @Test
+    @DisplayName("getGame failure")
+    public void getGameFailure() throws ResponseException {
+        Assertions.assertEquals(null, gameDAO.getGame(3));
+    }
+
+    @Test
+    @DisplayName("listGames success")
+    public void listGamesSuccess() throws ResponseException {
+        gameDAO.createGame("gameName");
+        Assertions.assertDoesNotThrow(()->gameDAO.listGames());
+        Assertions.assertNotNull(gameDAO.listGames());
+    }
+
+    @Test
+    @DisplayName("listGames failure")
+    public void listGamesFailure() throws ResponseException {
+        Collection<GameData> emptyList = new ArrayList<>();
+        Assertions.assertEquals(emptyList, gameDAO.listGames());
+    }
+
+    @Test
+    @DisplayName("updateGame success")
+    public void updateGameSuccess() throws ResponseException {
+        GameData game = gameDAO.createGame("gameName");
+        Assertions.assertDoesNotThrow(()->gameDAO.updateGame(
+                game.gameID(), ChessGame.TeamColor.WHITE, "username"));
+    }
+
+    @Test
+    @DisplayName("updateGame failure")
+    public void updateGameFailure() {
+        Assertions.assertThrows(NullPointerException.class, ()->
+        {gameDAO.updateGame(3, ChessGame.TeamColor.WHITE, "username");});
+    }
+
+    @Test
+    @DisplayName("deleteGame success")
+    public void deleteGameSuccess() throws ResponseException {
+        GameData game = gameDAO.createGame("gameName");
+        Assertions.assertDoesNotThrow(()->gameDAO.deleteGame(game.gameID()));
+    }
+
+    @Test
+    @DisplayName("clear success")
+    public void clearSuccess() throws ResponseException {
+        GameData game = gameDAO.createGame("gameName");
+        Assertions.assertDoesNotThrow(()->{gameDAO.clear();});
+    }
+
+
 }
