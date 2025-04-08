04e37a420a664d4da8bc199dc652f2cafad1bb86
Abigail Peterson <desrange@byu.edu>
2025-04-08 13:40:19 -0600

    now highlight legal moves only accepts correctly formatted input

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -7,10 +7,16 @@ public abstract class GameClient {
     //these two don't use the websocket and instead interact with the chessgame and BoardPrinter
 
     public String highlightLegalMoves(String... params) throws ResponseException {
-        // add code to make sure that even if there is the right number of parameters they are the right parameters
         if (params.length < 2) {
             return "did you mean \"highlight legal moves <START_POSITION>\"? Please try again.";
+        } else if (params.length < 3) {
+            throw new ResponseException(400, "Expected: highlight legal moves <START_POSITION>");
+        } else if (params[2].length() != 2) {
+            throw new ResponseException(400, "Expected: <START_POSITION> in form <letter#> ex: E2 or A8");
+        } else if (!Character.isAlphabetic(params[2].charAt(0)) || !Character.isDigit(params[2].charAt(1))) {
+            throw new ResponseException(400, "Expected: <START_POSITION> in form <letter#> ex: E2 or A8");
         } else {
+            //implement highlight legal moves
             return "highlight legal moves is under construction...";
         }
     }
