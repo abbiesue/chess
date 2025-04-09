6f68dd703f53b3680c991b11002e153c9855fe0b
Abigail Peterson <desrange@byu.edu>
2025-04-09 17:28:16 -0600

    reduced complexity in the Repl class

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,8 +1,7 @@
 package ui;
 
+import server.ResponseException;
 import server.ServerFacade;
-import websocket.ServerMessageObserver;
-import websocket.messages.*;
 
 import java.util.Scanner;
 
@@ -20,55 +19,83 @@ public class Repl {
         preloginClient = new PreloginClient(server);
     }
 
-    public void run () {
+    public void run() {
         System.out.println("\u265B Welcome to 240 Chess! Type \"help\" to get started. \u265B");
 
         Scanner scanner = new Scanner(System.in);
-        var result = "";
+        String result = "";
+
         while (!result.equals("quit")) {
             printPrompt();
             String line = scanner.nextLine();
+
             try {
                 result = preloginClient.eval(line);
-                System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
+                printResult(result);
+
                 if (result.contains("Logged in")) {
-                    while(!result.equals("logged out") && !result.equals("quit")) {
-                        printPrompt();
-                        line = scanner.nextLine();
-                        postloginClient = new PostloginClient(server, preloginClient.authToken);
-                        result = postloginClient.eval(line);
-                        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
-                        if (result.contains("joining")) {
-                            playerClient = new PlayerClient(postloginClient.authToken, server, serverURL);
-                            result = playerClient.eval(line);
-                            System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
-                            while(!result.contains("left")) {
-                                printPrompt();
-                                line = scanner.nextLine();
-                                result = playerClient.eval(line);
-                                System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
-                            }
-                        }
-                        if (result.contains("observing")) {
-                            // fix later
-                            observerClient = new ObserverClient(postloginClient.authToken,server, serverURL);
-                            result = observerClient.eval(line);
-                            System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
-                            while(!result.contains("left")) {
-                                printPrompt();
-                                line = scanner.nextLine();
-                                result = observerClient.eval(line);
-                                System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
-                            }
-                        }
-                    }
+                    handlePostLogin(scanner);
                 }
 
             } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
+                System.out.print(e.toString());
             }
         }
+    }
+
+
+    private void handlePostLogin(Scanner scanner) {
+        postloginClient = new PostloginClient(server, preloginClient.authToken);
+        String result = "";
+
+        while (!result.equals("logged out") && !result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = postloginClient.eval(line);
+                printResult(result);
+
+                if (result.contains("joining")) {
+                    handleGameAsPlayer(scanner, line);
+                } else if (result.contains("observing")) {
+                    handleGameAsObserver(scanner, line);
+                }
+
+            } catch (Throwable e) {
+                System.out.print(e.toString());
+            }
+        }
+    }
+
+    private void handleGameAsObserver(Scanner scanner, String line) throws ResponseException {
+        observerClient = new ObserverClient(postloginClient.authToken, server, serverURL);
+        String result = observerClient.eval(line);
+        printResult(result);
+
+        while (!result.contains("left")) {
+            printPrompt();
+            line = scanner.nextLine();
+            result = observerClient.eval(line);
+            printResult(result);
+        }
+    }
+
+    private void handleGameAsPlayer(Scanner scanner, String line) throws ResponseException {
+        playerClient = new PlayerClient(postloginClient.authToken, server, serverURL);
+        String result = playerClient.eval(line);
+        printResult(result);
+
+        while (!result.contains("left")) {
+            printPrompt();
+            line = scanner.nextLine();
+            result = playerClient.eval(line);
+            printResult(result);
+        }
+    }
+
+    private void printResult(String result) {
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
     }
 
     private void printPrompt() {
