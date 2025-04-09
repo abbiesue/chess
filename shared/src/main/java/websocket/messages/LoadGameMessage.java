6f5dd8468f7c739dde1b64751b286f3fbca876bb
Abigail Peterson <desrange@byu.edu>
2025-04-09 11:49:31 -0600

    added get methods to my ServerMessage child classes

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -3,11 +3,20 @@ package websocket.messages;
 import chess.ChessGame;
 
 public class LoadGameMessage extends ServerMessage {
-    ServerMessageType type = ServerMessageType.LOAD_GAME;
     ChessGame game;
+    ChessGame.TeamColor printColor;
 
-    public LoadGameMessage(ChessGame game) {
+    public LoadGameMessage(ChessGame game, ChessGame.TeamColor printColor) {
         super(ServerMessageType.LOAD_GAME);
         this.game = game;
+        this.printColor = printColor;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public ChessGame.TeamColor getPrintColor() {
+        return printColor;
     }
 }
--- shared/src/main/java/websocket/messages/NotificationMessage.java
+++ shared/src/main/java/websocket/messages/NotificationMessage.java
@@ -1,11 +1,14 @@
 package websocket.messages;
 
 public class NotificationMessage extends ServerMessage{
-    ServerMessageType type = ServerMessageType.NOTIFICATION;
     String message;
 
     public NotificationMessage(String message) {
         super(ServerMessageType.NOTIFICATION);
         this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
